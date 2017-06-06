51361c2fd560b072015484d9388cde05b527429a
kmsheng <kmsh3ng@gmail.com>
2017-06-06 22:50:21 +0800

    make logger work better

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,26 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import {createLogger} from 'redux-logger';
 import rootReducer from './modules/reducer';
+import {Iterable} from 'immutable';
+
+const middlewares = [thunk];
+
+if ('development' === process.env.NODE_ENV) {
+  const logger = createLogger({
+    stateTransformer: (state) => {
+      if (Iterable.isIterable(state)) {
+        return state.toJS();
+      }
+      return state;
+    }
+  });
+  middlewares.push(logger);
+}
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk, logger)
+  applyMiddleware(...middlewares)
 );
 
 export default store;

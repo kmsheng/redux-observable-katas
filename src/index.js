b48b5c5b1bcce6e8aa36d99a34c112e9e3b34ef3
kmsheng <kmsh3ng@gmail.com>
2017-06-06 21:40:31 +0800

    create helpers folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import App from './containers/App/App';
+import registerServiceWorker from './helpers/registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
